9959131bc062c2ab2bff89929a54ed39c2de2e55
Fatima Ezzahra Legchayri <fatimaezzahra.legchayri@gmail.com>
2024-10-12 18:45:46 +0200

    save tasks to local storage

--- script.js
+++ script.js
@@ -1,54 +1,52 @@
 const addButton = document.getElementById('add-task-btn')
-const taskInput = document.getElementById('task-input')
+const input = document.getElementById('task-input')
 const taskList = document.getElementById('task-list')
 
 document.addEventListener('DOMContentLoaded',()=>{
-    // addTask()
-    loadTasks();
+    display()
 })
 
-function addTask(taskText, save = true){
-        const taskText = taskInput.value.trim()
-        
-        if (!taskText) {
-            taskText = taskInput.value.trim();
-        }
-        if(taskText == ''){
+let arr = JSON.parse(localStorage.getItem('tasks')) || [];
+function addTask(){
+    taskList.innerHTML = ''
+        const taskInput = input.value.trim()
+        if(taskInput == ''){
             alert('enter a task')
-            return;
-        }else{
-            const li = document.createElement('li');
-            li.textContent = taskText;
-            const removeBtn = document.createElement('button')
-            removeBtn.textContent = 'Remove'
-            removeBtn.classList.add('remove-btn')
-
-            li.appendChild(removeBtn)
-            taskList.appendChild(li)
-            taskInput.value = ''
-            
-            removeBtn.onclick = function(){
-                taskList.removeChild(li)
-            }
-            const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-            const updatedTasks = storedTasks.filter(storedTask => storedTask !== taskText);
-            localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-        }  
-        if (save) {
-            const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-            storedTasks.push(taskText);
-            localStorage.setItem('tasks', JSON.stringify(storedTasks));
-        }  
+        }else{ 
+            arr.push(taskInput)
+            console.log(arr,'lll');
+            input.value = ''
+            display()
+        }
+    localStorage.setItem('tasks',JSON.stringify(arr))   
 }
+
 addButton.addEventListener('click',addTask)
-taskInput.addEventListener('keypress',function (event){
+input.addEventListener('keypress',function (event){
     if(event.key === 'Enter'){
         addTask()
     }
 })
 
-function loadTasks() {
-    const storedTasks = JSON.parse(localStorage.getItem('tasks') || '[]');
-    storedTasks.forEach(taskText => addTask(taskText, false)); // 'false' indicates not to save again to Local Storage
+function display(){
+    if(arr.length > 0){
+        arr.forEach(element=>{
+           const li = document.createElement('li')
+           const btn = document.createElement('button')
+           btn.classList.add('remove-btn')
+           btn.textContent = 'remove'
+           li.textContent = element
+           li.appendChild(btn)
+           taskList.appendChild(li)
+    
+           btn.onclick = function remove(index){
+            taskList.removeChild(li)
+            arr.splice(index,1)
+            localStorage.setItem('tasks',JSON.stringify(arr))
+            }
+        })
+    }else{
+        taskList.textContent = 'no tasks for today'
+    }
 }
-   
+ 
